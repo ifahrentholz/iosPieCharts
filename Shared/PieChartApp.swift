e3048540954bb601c4608c256506b7fd2d80667c
Ingo Fahrentholz <ingof.work@gmail.com>
2021-02-13 23:20:39 +0100

    Initial Commit

---
+++ Shared/PieChartApp.swift
@@ -0,0 +1,17 @@
+//
+//  PieChartApp.swift
+//  Shared
+//
+//  Created by Ingo Fahrentholz on 13.02.21.
+//
+
+import SwiftUI
+
+@main
+struct PieChartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
